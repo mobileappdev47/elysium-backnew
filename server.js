1f7348ff781343b686443a6a6395926c10431510
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2024-06-24 12:19:02 +0530

    cors url set

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const basePaperDataRouter = require('./routes/basePaperDataRoute.js')
 dbConnect();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://elysium-frountendnew.vercel.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
