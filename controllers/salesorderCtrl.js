78e46f1ced69d59284c66101d6e84f4dee147ed7
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2024-06-29 14:25:33 +0530

    console remove

--- controllers/salesorderCtrl.js
+++ controllers/salesorderCtrl.js
@@ -543,8 +543,7 @@ const doneUpdateSalesOrder = asyncHandler(async (req, res) => {
             // Check roll quantity
             for (const product of salesOrder.products) {
                 const productQrEntries = salesOrder.qualityqrs.filter(qr => qr.productName === product.productname);
-                console.log(productQrEntries, 'roll');
-                console.log(product.rollqty, 'rollqty');
+               
 
                 // if (product.rollqty !== productQrEntries.length) {
                 //     return res.status(400).json({
@@ -568,7 +567,6 @@ const doneUpdateSalesOrder = asyncHandler(async (req, res) => {
                     mismatchedProducts.push(qrProduct.uniqueid);
                 }
             }
-            console.log(mismatchedProducts, 'mismatch');
             if (mismatchedProducts.length > 0) {
                 return res.status(400).json({
                     success: false,
